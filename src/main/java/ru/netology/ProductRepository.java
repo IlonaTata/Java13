073c4d9daa9bce61f729a9aceed2e67055d76025
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2022-11-19 10:15:48 +0300

    Initial coomit

---
+++ src/main/java/ru/netology/ProductRepository.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+public class ProductRepository {
+    private Product[] types = new Product[0];
+
+    public void save(Product type) {
+        Product[] tmp = new Product[types.length + 1];
+        for (int i = 0; i < types.length; i++) {
+            tmp[i] = types[i];
+        }
+        tmp[tmp.length - 1] = type;
+        types = tmp;
+    }
+    public  Product[] findAll() {
+        return types;
+    }
+   public void removeById(int id) {
+       Product[] tmp = new Product[types.length - 1];
+       int copyToIndex = 0;
+       for (Product type : types) {
+           if (type.getId() != id) {
+               tmp[copyToIndex] = type;
+               copyToIndex++;
+            }
+       }
+       types = tmp;
+    }
+
+
+}
---
+++ src/test/java/ProductRepositoryTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.Book;
+import ru.netology.Product;
+import ru.netology.ProductRepository;
+import ru.netology.Smartphone;
+
+public class ProductRepositoryTest {
+    Product type1 = new Product(1, ":Жизнь", 200);
+    Product type2 = new Book(2, ":Учеба", 300, "Акунин");
+    Product type3 = new Smartphone(3, ":Телефон", 400, "Москва");
+
+
+    @Test
+    public void shouldSetId() {
+        ProductRepository repo = new ProductRepository();
+        repo.save(type1);
+        repo.save(type2);
+        repo.save(type3);
+        repo.findAll();
+        Product[] expected = {type1, type2, type3};
+        Product[] actual = repo.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldSave() {
+        ProductRepository repo = new ProductRepository();
+        repo.save(type1);
+        repo.save(type2);
+        repo.save(type3);
+        repo.findAll();
+        Product[] expected = {type1, type2, type3};
+        Product[] actual = repo.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldRemoveById(){
+        ProductRepository repo = new ProductRepository();
+        repo.save(type1);
+        repo.save(type2);
+        repo.save(type3);
+        repo.findAll();
+        repo.removeById(2);
+        Product[]expected={type1, type3};
+        Product[]actual=repo.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
